1aa1c99431b200eaefc9b690b97a16838b48a55a
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-12-04 00:03:29 +0300

    dz6_1

--- dz6_1/Program.cs
+++ dz6_1/Program.cs
@@ -2,4 +2,44 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3*/
 
-
+int [] Array (int length, int start, int end)
+    {
+    Console.Write("Введите желаемое колиство чисел для подсчета - ");
+    length = int.Parse(Console.ReadLine());
+    int [] array = new int [length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите число {i}: ");
+        array[i] = int.Parse(Console.ReadLine());                    
+    }
+        return array;            
+    }
+int NumberOfPositiv (int [] array)
+    {
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+        return count;
+    }
+void PrintArray(int [] array)
+    {
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+    }
+int [] newArray = Array( 5,5,5);
+PrintArray(newArray);
+int countNumber = NumberOfPositiv(newArray);
+Console.WriteLine ($"Введено {countNumber} числа(ел) больше нуля");
